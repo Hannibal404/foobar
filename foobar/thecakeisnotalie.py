b936fb3ec2a1a48240eb91b7a4273ecf1d0dce09
Hannibal404 <54623625+Hannibal404@users.noreply.github.com>
2020-06-23 12:35:30 +0530

    Delete thecakeisnotalie.py

--- foobar/thecakeisnotalie.py
+++
@@ -1,8 +0,0 @@
-def answer(s):
-    try:
-        return max([s.count(s[:x]) for x in range(len(s)) if s[:x]*s.count(s[:x]) == s])
-    except Exception:
-        return 1
-print(answer("abcabcabcabc"))
-print(answer("abccbaabccba"))
-print(answer("abccbaabccb"))
